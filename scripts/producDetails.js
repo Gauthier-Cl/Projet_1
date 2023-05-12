a8c71fa8a4a578ed89384ecbfd199a03f43fa8f3
Gauthier.Cl <gauthier.cla@free.fr>
2023-05-12 16:25:34 +0200

    responsive productDetails

--- scripts/producDetails.js
+++ scripts/producDetails.js
@@ -5,7 +5,7 @@ const listBike = [
     price: "$99",
     image: "./assets/img/bike1.png",
     description:
-      "Passez difficilement et rapidement de 25 à 45 km/h à tout moment en appuyant seulement sur un bouton ! Remarque : Ce vélo est livré avec l'option offrant la possibilité de basculer entre un vélo électrique classique et un speed bike dans les fonctions intégrées sur l'écran. Le vélo est livré avec les réglages par défaut sur 25 km/h et si vous le laissez avec ce réglage, ce vélo n'a donc pas besoin d'être immatriculé ou assuré en tant que Speed Bike. Pour cette raison, le vélo n'est pas équipé d'un CoC pour l'enregistrer en tant que Speed Bike et la fonction speed bike est uniquement recommandée pour une utilisation sur routes privées ",
+      "Passez facilement et rapidement de 25 à 45 km/h à tout moment en appuyant seulement sur un bouton ! Remarque : Ce vélo est livré avec l'option offrant la possibilité de basculer entre un vélo électrique classique et un speed bike dans les fonctions intégrées sur l'écran. Le vélo est livré avec les réglages par défaut sur 25 km/h et si vous le laissez avec ce réglage, ce vélo n'a donc pas besoin d'être immatriculé ou assuré en tant que Speed Bike. Pour cette raison, le vélo n'est pas équipé d'un CoC pour l'enregistrer en tant que Speed Bike et la fonction speed bike est uniquement recommandée pour une utilisation sur routes privées ",
   },
   {
     id: "1",
@@ -69,34 +69,3 @@ model.textContent = bikeBis.model;
 
 const featured = document.getElementById("featured");
 featured.src = bikeBis.image;
-// Carousel
-/*
-const thumbnails = document.getElementsByClassName("thumbnail");
-thumbnails.src = bikeBis.image;
-const activeImages = document.getElementsByClassName("active");
-activeImages.src = bikeBis.image;
-const featured = document.getElementById("featured");
-featured.src = bikeBis.image;
-const buttonRight = document.getElementById("slideRight");
-const buttonLeft = document.getElementById("slideLeft");
-
-for (let i = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener("mouseover", () => {
-    console.log(activeImages);
-
-    if (activeImages.length > 0) {
-      activeImages[0].classList.remove("active");
-    }
-
-    this.classList.add("active");
-    document.getElementById("featured").src = this.src;
-  });
-}
-
-buttonLeft.addEventListener("click", () => {
-  document.getElementById("slider").scrollLeft -= 180;
-});
-
-buttonRight.addEventListener("click", () => {
-  document.getElementById("slider").scrollLeft += 180;
-});*/
--- scripts/script.js
+++ scripts/script.js
@@ -115,5 +115,5 @@ const cards = document.querySelectorAll(".news");
 
 cards.forEach((card, index) => {
   const link = card.querySelector("a");
-  link.href = `http://127.0.0.1:5500/productDetails.html?bikeId=${index}`;
+  link.href = `productDetails.html?bikeId=${index}`;
 });
